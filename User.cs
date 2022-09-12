23022af6d2ccb3b0f30ecc1e9683781508ddb2c9
Pasha <test@example.com>
2022-09-12 15:17:24 +0300

    Добавьте файлы проекта.

---
+++ User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public static class User
+    {
+        public static string FirstName { get; set; }
+        public static string DoubleName { get; set; }
+        public static string LastName { get; set; }
+    }
+}
